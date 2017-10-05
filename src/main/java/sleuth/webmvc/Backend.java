fcd8aef1829f46dc3ef2060d53bc7c05205193b5
Adrian Cole <acole@pivotal.io>
2017-10-05 21:49:01 +0800

    Reports spans to xray

--- src/main/java/sleuth/webmvc/Backend.java
+++ src/main/java/sleuth/webmvc/Backend.java
@@ -3,12 +3,23 @@ package sleuth.webmvc;
 import java.util.Date;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.cloud.sleuth.zipkin2.ZipkinAutoConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Primary;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import zipkin2.Span;
+import zipkin2.reporter.Reporter;
+import zipkin2.reporter.xray_udp.XRayUDPReporter;
 
 @EnableAutoConfiguration
 @RestController
 public class Backend {
+
+  @Bean(ZipkinAutoConfiguration.REPORTER_BEAN_NAME)
+  Reporter<Span> spanReporter() {
+    return XRayUDPReporter.create();
+  }
 
   @RequestMapping("/api") public String printDate() {
     return new Date().toString();
