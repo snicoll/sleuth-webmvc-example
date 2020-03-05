63ddf8d9a40f37acbd7c01f21505b02a27bf54da
Adrian Cole <acole@pivotal.io>
2020-03-05 18:00:25 +0800

    Shows how to trace Spring WebFlux requests
    This changes the example to serve requests with reactive `Mono`
    controllers, using `WebClient` instead of `RestTemplate` to make the
    call from the frontend to the backend.

--- src/main/java/sleuth/webmvc/Backend.java
+++ src/main/java/sleuth/webmvc/Backend.java
@@ -5,13 +5,14 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Mono;
 
 @EnableAutoConfiguration
 @RestController
 public class Backend {
 
-  @RequestMapping("/api") public String printDate() {
-    return new Date().toString();
+  @RequestMapping("/api") public Mono<String> printDate() {
+    return Mono.fromSupplier(() -> new Date().toString());
   }
 
   public static void main(String[] args) {
--- src/main/java/sleuth/webmvc/Frontend.java
+++ src/main/java/sleuth/webmvc/Frontend.java
@@ -7,23 +7,24 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.client.RestTemplate;
+import org.springframework.web.reactive.function.client.WebClient;
+import reactor.core.publisher.Mono;
 
 @EnableAutoConfiguration
 @RestController
 @CrossOrigin // So that javascript can be hosted elsewhere
 public class Frontend {
 
-  @Autowired RestTemplate restTemplate;
+  @Autowired WebClient webClient;
 
   String backendBaseUrl = System.getProperty("spring.example.backendBaseUrl", "http://localhost:9000");
 
-  @RequestMapping("/") public String callBackend() {
-    return restTemplate.getForObject(backendBaseUrl + "/api", String.class);
+  @RequestMapping("/") public Mono<String> callBackend() {
+    return webClient.get().uri("/api").retrieve().bodyToMono(String.class);
   }
 
-  @Bean RestTemplate restTemplate() {
-    return new RestTemplate();
+  @Bean WebClient webClient() {
+    return WebClient.create(backendBaseUrl);
   }
 
   public static void main(String[] args) {
